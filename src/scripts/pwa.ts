5cbe89ae72ba77ee6424f983ef3714c72d05b5fb
vedicastrology123 <vedicastrology123@gmail.com>
2025-10-16 20:44:41 +0530

    29th commit

---
+++ src/scripts/pwa.ts
@@ -0,0 +1,7 @@
+// src/scripts/pwa.ts
+import type { ManifestOptions } from "vite-plugin-pwa";
+import { registerSW } from "virtual:pwa-register";
+
+export const manifestOptions: Partial<ManifestOptions> = {
+  // Your manifest options here
+};
