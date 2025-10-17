9d4a127fd87fb4a1319403f65b01335ba51b872a
vedicastrology123 <vedicastrology123@gmail.com>
2025-10-17 14:01:41 +0530

    31st commit

--- src/components/Q2a.jsx
+++ src/components/Q2a.jsx
@@ -12,4 +12,33 @@ export const JsonQ2AData = () => {
 	))
   )}
 
+function Dropdown(key, commaSeparatedValues) {
+	const [selectedValue, setSelectedValue] = useState('');
+
+	const handleChange = (event) => {
+		setSelectedValue(event.target.value);
+	};
+
+	const valuesArray = commaSeparatedValues.split(',');
+	const selectElement = document.getElementById('my-dropdown'); // Assuming your select has id="myDropdown"
+	const option = document.createElement('option');
+	option.label = 'Please choose the correct answer: ';
+	option.value = 'Choice...';
+	selectElement.appendChild(option);
+	valuesArray.forEach(value => {
+		const trimmedValue = value.trim();
+		option.value = trimmedValue;
+		option.textContent = trimmedValue;
+		selectElement.appendChild(option);
+	});
+	selectElement.addEventListener('change', handleChange);
+	{selectedValue && <p>You selected: {selectedValue}</p>}
+
+	// return (
+	// <div>
+	// 	<select id="my-dropdown" value={selectedValue} onChange={handleChange}>
+	// 	</select>
+	// </div>
+	// );
+}
   export default JsonQ2AData;
