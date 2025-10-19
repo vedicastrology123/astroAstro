59f80cb027d0b048d5e77e177fb5e9abe8f992ca
vedicastrology123 <vedicastrology123@gmail.com>
2025-10-19 20:51:30 +0530

    39th commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stevehora.astrogram',
+  appName: 'astrogram',
+  webDir: 'build'
+};
+
+export default config;
