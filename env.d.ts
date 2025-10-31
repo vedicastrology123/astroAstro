72bef5f55cccd7e7252a02b24093a71b3eae661b
vedicastrology123 <vedicastrology123@gmail.com>
2025-10-31 07:30:03 +0530

    63rd commit

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
-
-interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
