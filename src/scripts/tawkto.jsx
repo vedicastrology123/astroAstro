d5167e5331f982b83ead1eccae5a1a90809cb1c4
vedicastrology123 <vedicastrology123@gmail.com>
2025-12-11 18:31:13 +0530

    tawk.to

---
+++ src/scripts/tawkto.jsx
@@ -0,0 +1,11 @@
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/693ab9436a7e4919819a2b8c/1jc6m7ch7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
