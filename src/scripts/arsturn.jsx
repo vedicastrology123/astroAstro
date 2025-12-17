912c366e850adea7d1309ed6a09d1888e8c04728
vedicastrology123 <vedicastrology123@gmail.com>
2025-12-17 09:11:08 +0530

    widget

---
+++ src/scripts/arsturn.jsx
@@ -0,0 +1,9 @@
+
+window.username="vedicastrology";
+window.widgetColor="white";
+window.widgetBackgroundColor="#4a90e2";
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.async = true;
+s.src = "https://www.arsturn.com/widget.js";
+document.head.appendChild(s);
